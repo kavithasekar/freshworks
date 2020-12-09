362628854f171d7fee90817c5982693ba7a8e32d
kavithasekar <ssikavitha2000@gmail.com>
2020-12-09 23:40:58 +0530

    final commit

--- sample.js
+++
@@ -1,6 +0,0 @@
-const express=require('express')
-const fs=require('fs');
-let data = fs.readFileSync('data.json').toString();
-let data1 = JSON.parse(data);
-let e=data1['key1']["ttl"];
-let seconds = new Date().getTime() / 1000;
